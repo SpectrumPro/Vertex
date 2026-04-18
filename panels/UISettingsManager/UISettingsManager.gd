4622eefad0e2714c0a4cffdb1e83c6a40fae9acf
Liam <git@liamsherwin.com>
2026-04-18 16:06:07 +1200

    Enable the use of ComponentButton in UISettingsManager

--- panels/UISettingsManager/UISettingsManager.gd
+++ panels/UISettingsManager/UISettingsManager.gd
@@ -13,7 +13,10 @@ class_name UISettingsManager extends UIPanel
 @export var settings_manager_view: SettingsManagerView
 
 
-## The current selected component
+## The current selected GBC component
+var _component: Object
+
+## The current selected SettingsManager
 var _manager: SettingsManager
 
 
@@ -23,16 +26,35 @@ func _init(p_uuid: String = UUID.v4(), ...p_args: Array[Variant]) -> void:
 	_set_class_name("UISettingsManager")
 
 
+## Sets the component to display
+func set_component(p_component: Object) -> void:
+	_component = p_component
+	component_button.set_component(p_component)
+	
+	if not is_instance_valid(p_component):
+		return
+	
+	set_manager(p_component.get_settings())
+
+
 ## Sets the component
 func set_manager(p_manager: SettingsManager) -> void:
+	if p_manager.get_owner() != _component:
+		set_component(p_manager.get_owner())
+		return
+	
 	_manager = p_manager
-	component_button.set_text(_manager.get_inheritance_child())
 	
 	if not is_instance_valid(_manager):
 		settings_manager_view.reset()
 		return
 	
 	settings_manager_view.set_manager(_manager)
+
+
+## Returns the current selected GBC component
+func get_component() -> Object:
+	return _component
 
 
 ## Gets the current component
